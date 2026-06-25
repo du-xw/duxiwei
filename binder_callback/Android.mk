dfd746449aee07ba40fafa21570e3fde2fce0ae2
duxiwei <duxiwei@aliyun.com>
2026-06-25 14:12:07 +0800

    add binder_callback

---
+++ binder_callback/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= client.cpp
+LOCAL_SHARED_LIBRARIES :=liblog libcutils libbinder libutils libhardware
+LOCAL_MODULE:= client_binder_Callback
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= server.cpp
+LOCAL_SHARED_LIBRARIES :=liblog libcutils libbinder libutils libhardware
+LOCAL_MODULE:= server_binder_Callback
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_EXECUTABLE)
--- build.mk
+++ build.mk
@@ -1,3 +1,5 @@
 $(warning "duxiwei build vendor dir")
 
 PRODUCT_PACKAGES += Mytest
+PRODUCT_PACKAGES += client_binder_Callback
+PRODUCT_PACKAGES += server_binder_Callback
